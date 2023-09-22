1a29e9b937929f7119bf091ff72e750dd7561cdc
Дмитрий Дементеев <75799687+Dmitrij05@users.noreply.github.com>
2023-09-22 23:41:01 +0300

    Add files via upload

---
+++ PR3 - 14 задание.py
@@ -0,0 +1,14 @@
+x0 = int(input())
+y0 = int(input())
+x1 = 0
+y1 = 0
+x2 = 0
+y2 = 5
+x3 = 5
+y3 = 0
+x4 = 5
+y4 = 5
+if (x0 > x1 and x0 < x3 and y0 > y1 and y0 < y4):
+    print('Попадает')
+else:
+    print('Не попадает')
